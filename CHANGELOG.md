a52f77d893e07dbfca9d850faed22fada22299b0
Tomasz Setkowski <tom@ai-traders.com>
2019-04-27 18:34:38 +0000

    initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+### 0.1.0 (2019-Apr-27)
+
+Initial release - alpine and ubuntu18 with docker in docker.
---
+++ test/integration/test_dojo_work/test_docker.py
@@ -0,0 +1,25 @@
+import os
+import subprocess
+
+def decode_utf8(bytes):
+    return bytes.decode('utf-8')
+
+def run_command(exe, args):
+    result = subprocess.run([exe] + args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    result.stdout = decode_utf8(result.stdout)
+    result.stderr = decode_utf8(result.stderr)
+    return result
+
+def test_docker_ps():
+    result = run_command('docker', ['ps'])
+    assert 'CONTAINER ID' in result.stdout
+    assert result.returncode == 0
+
+def test_docker_run():
+    result = run_command('docker', ['run', 'hello-world'])
+    assert 'Hello from Docker!' in result.stdout
+    assert result.returncode == 0
+
+def test_docker_compose_cli():
+    result = run_command('docker-compose', ['--version'])
+    assert result.returncode == 0
