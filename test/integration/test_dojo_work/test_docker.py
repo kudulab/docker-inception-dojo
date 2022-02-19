874fbf3f30dcfa5bc8e49e96794969406a174211
xmik <ewa@kudulab.io>
2022-02-19 15:03:16 +0000

    add test for pwd

--- test/integration/test_dojo_work/test_docker.py
+++ test/integration/test_dojo_work/test_docker.py
@@ -10,6 +10,11 @@ def run_command(exe, args):
     result.stderr = decode_utf8(result.stderr)
     return result
 
+def test_current_directory_is_dojo_work():
+    result = run_command('pwd', [])
+    assert '/dojo/work' in result.stdout
+    assert result.returncode == 0
+
 def test_docker_ps():
     result = run_command('docker', ['ps'])
     assert 'CONTAINER ID' in result.stdout
